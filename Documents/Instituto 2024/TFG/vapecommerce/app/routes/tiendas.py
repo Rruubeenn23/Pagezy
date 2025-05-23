844219857a4f684dc9a29d1ea4fb9682721fdfb0
Rruubeenn23 <rubencereceda23@gmail.com>
2025-05-23 16:31:25 +0200

    Deploy Final

--- Documents/Instituto 2024/TFG/vapecommerce/app/routes/tiendas.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from app.models_publicos import TiendaPublica
-
-tiendas_bp = Blueprint("tiendas", __name__, url_prefix="/tiendas")
-
-@tiendas_bp.route("/")
-def lista_tiendas():
-    tiendas = TiendaPublica.query.all()
-    return render_template("PaginasPresentacion/tiendas/lista_tiendas.html", tiendas=tiendas)
