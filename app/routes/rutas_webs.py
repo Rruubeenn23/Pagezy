6adbda82eadad12f7de05490a46375d91600013b
Rruubeenn23 <rubencereceda23@gmail.com>
2025-05-25 14:23:31 +0200

    Configuracion Final

--- app/routes/rutas_webs.py
+++ app/routes/rutas_webs.py
@@ -19,8 +19,7 @@ def ver_tienda(nombre_tienda):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
     tienda_proceso = TiendaEnProceso.query.filter_by(nombre_tienda=nombre_tienda).first()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -71,9 +70,7 @@ from flask import request
 @tienda_bp.route("/<nombre_tienda>/productos/<int:pagina>")
 def ver_productos(nombre_tienda, pagina=1):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -166,9 +163,7 @@ def ver_info(nombre_tienda):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
     tienda_proceso = TiendaEnProceso.query.filter_by(nombre_tienda=nombre_tienda).first()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
 
     metadata = MetaData()
@@ -207,9 +202,7 @@ def ver_producto(nombre_tienda, producto_id):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
     tienda_proceso = TiendaEnProceso.query.filter_by(nombre_tienda=nombre_tienda).first()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -255,9 +248,7 @@ def guardar_pedido(nombre_tienda):
     direccion = data.get("direccion", "").strip()
 
     tienda = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -332,9 +323,7 @@ def guardar_pedido(nombre_tienda):
 @tienda_bp.route("/<nombre_tienda>/login", methods=["GET", "POST"])
 def login(nombre_tienda):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -412,9 +401,7 @@ def login(nombre_tienda):
 def register(nombre_tienda):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -491,9 +478,7 @@ def admin_panel(nombre_tienda):
     tienda_publica = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
     tienda_proceso = TiendaEnProceso.query.filter_by(nombre_tienda=nombre_tienda).first()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -537,9 +522,7 @@ def eliminar_producto(nombre_tienda, producto_id):
         return redirect(url_for("tienda_publica.login", nombre_tienda=nombre_tienda))
 
     tienda = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda_publica.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -563,9 +546,7 @@ def eliminar_pedido(nombre_tienda, pedido_id):
         return redirect(url_for("tienda_publica.login", nombre_tienda=nombre_tienda))
 
     tienda = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -591,9 +572,7 @@ def actualizar_estado_pedido(nombre_tienda, pedido_id):
     nuevo_estado = request.form.get("estado")
 
     tienda = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -621,9 +600,7 @@ def eliminar_cliente(nombre_tienda, cliente_id):
         return redirect(url_for("tienda_publica.login", nombre_tienda=nombre_tienda))
 
     tienda = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -648,9 +625,7 @@ def cuenta(nombre_tienda):
     tienda = TiendaPublica.query.filter_by(nombre_tienda=nombre_tienda).first_or_404()
     tienda_proceso = TiendaEnProceso.query.filter_by(nombre_tienda=nombre_tienda).first()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.abspath(os.path.join(BASE_DIR, tienda.ruta_db))
-
+    db_path = os.path.join(os.getcwd(), tienda.ruta_db)
     engine = create_engine(f"sqlite:///{db_path}")
     metadata = MetaData()
     metadata.reflect(bind=engine)
